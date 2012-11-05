b812e7c14f81c1036dc12b64ddde35978f157d3d
Casen <cdubd10@gmail.com>
2012-11-04 21:58:30 -0800

    adding default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTemplate::Application.config.secret_token = '0d791db57bf653bc87e922736af1a58e58f6c7a75131fc9041dd64fbb96778f4a04109d6039a8b003d097af24782f7950db9de86cf14174fd683af9f7d2bc114'
